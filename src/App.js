49f94fdc75ad7d525149b1e3d71126a87da23315
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-02-18 05:31:16 +0100

    Created a new repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
+import Images from './images';
+import { useEffect, useState, useRef } from 'react';
+// import uniqid from 'uniqid';`
 
 function App() {
+  const [view, setView] = useState('active');
+  const [score, setScore] = useState(0);
+  const [images, setImage] = useState([]);
+  const [level, setLevel] = useState(1);
+  let  imageLength = useRef(4);
+  let count = useRef(0);
+
+
+  useEffect(() => {
+    if (view !== 'active') return;
+    const imagesArr = [];
+    
+    while (imagesArr.length < imageLength.current) {
+      let n = Math.floor(Math.random() * Images.length);
+      !imagesArr.includes(n) && imagesArr.push(n);
+    }
+
+    setImage(imagesArr);
+  }, [ view, level ])
+
+  useEffect(() => {
+    if (score === 0) return;
+    count.current++;
+    console.log(count.current);
+    console.log(imageLength.current);
+    if (count.current === imageLength.current) {
+      console.log('equal'
+      );
+      imageLength.current += 2;
+      // setView('false');
+      setLevel(l => l + 1);
+      // restart();
+      count.current = 0;
+      return;
+    }
+    let ar = []
+    while (ar.length < imageLength.current) {
+      let n = Math.floor(Math.random() * imageLength.current);
+      !ar.includes(n) && ar.push(n);
+    }
+    setImage(pImg => ar.map(n => pImg[n]));
+  }, [ score ])
+
+
+  const restart = e => {
+    setView('active');
+    imageLength.current = 4;
+    setScore(0);
+    setLevel(0);
+
+    count.current = 0;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        {
+          images.map((n, index) =>
+            <Card
+              key={Images[n].index}
+              src={Images[n].src}
+              name={Images[n].name}
+              setView={setView}
+              setScore={setScore}
+              view={view}
+              level={level}
+            />)
+        }
+      </div>
+      {view === 'inactive' && <div className='result'>
+        <div>
+          <p>Game Over</p>
+          <p>Score: {score}</p>
+          <button onClick={restart}>Restart</button>
+        </div>
+      </div>
+      }
     </div>
   );
+}
+
+function Card(props) {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (props.view === 'inactive') return;
+    setClicked(false);
+  }, [ props.view, props.level ])
+
+  const cardEvent = e => {
+    if (clicked === true) {
+      props.setView('inactive');
+      return;
+    }
+    setClicked(true);
+    props.setScore(prev => prev+1)
+  }
+
+  return (
+    <div className='card'>
+      <button onClick={cardEvent}>
+        <img src={props.src} alt='tanjiro' />
+        <p>{props.name}</p>
+      </button>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/images/index.js
@@ -0,0 +1,65 @@
+import allMight from './all might.jpg';
+import Atsu from './Atsu.jpg';
+import bakugo from './bakugo.jpg';
+import deku from './deku.jpg';
+import gabi from './gabi.jpg';
+import hanima from './hanima.png';
+import tanjiro from './tanjiro.png';
+import Ochaco from './Ochaco.jpg';
+import toga from './toga.jpg';
+import zoya from './zoya.jpg';
+
+const Images = [
+    {
+        index: 0,
+        name: 'all might',
+        src: allMight
+    },
+    {
+        index: 1,
+        name: 'Atsu',
+        src: Atsu
+    },
+    {
+        index: 2,
+        name: 'bakugo',
+        src: bakugo
+    },
+    {
+        index: 3,
+        name: 'deku',
+        src: deku
+    },
+    {
+        index: 4,
+        name: 'gabi',
+        src: gabi
+    },
+    {
+        index: 5,
+        name: 'hanima',
+        src: hanima
+    },
+    {
+        index: 6,
+        name: 'tanjiro',
+        src: tanjiro
+    },
+    {
+        index: 7,
+        name: 'Ochaco',
+        src: Ochaco
+    },
+    {
+        index: 8,
+        name: 'toga',
+        src: toga
+    },
+    {
+        index: 9,
+        name: 'zoya',
+        src: zoya
+    }
+]
+
+export default Images;
